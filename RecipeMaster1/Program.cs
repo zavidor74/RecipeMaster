c719840e69bb647e9556d36df0944b50a0beafc1
Zvi Avidor2 <zavidor@microsoft.com>
2022-07-29 20:33:40 +0300

    first

---
+++ RecipeMaster1/Program.cs
@@ -0,0 +1,191 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using RecipeMaster1;
+
+Console.WriteLine("Hello, World!");
+
+Ingredient flour = new Ingredient
+{
+    Id = "14d1f940-5476-45ff-8e8a-4893e068affa",
+    Name = "Flour",
+    UnitType = UnitType.Gram
+};
+
+Ingredient sugarPowder = new Ingredient
+{
+    Id = "015ecab0-bf5e-4a74-ba7d-e304cb663ceb",
+    Name = "Sugar Powder",
+    UnitType = UnitType.Gram
+};
+
+Ingredient salt = new Ingredient
+{
+    Id = "804785cb-23a3-41ed-a82a-1dcefbbc066e",
+    Name = "Salt",
+    UnitType = UnitType.Tbsp
+};
+
+Ingredient butter = new Ingredient
+{
+    Id = "43d90ccd-4b31-45a1-953f-ab93004afee6",
+    Name = "Butter",
+    UnitType = UnitType.Gram
+};
+
+Ingredient egg = new Ingredient
+{
+    Id = "baca1e9b-2807-4b01-a845-58b974488cc7",
+    Name = "Large Egg",
+    UnitType = UnitType.Item
+};
+
+Ingredient lemonJuice = new Ingredient
+{
+    Id = "824f1995-6e11-4459-ab44-8596800e0fb5",
+    Name = "Lemon Juice",
+    UnitType = UnitType.Gram
+};
+
+Recipe pastryDough = new Recipe()
+{
+    Id = "623d450a-2327-4405-b290-4956e7f2eecb",
+    Name = "Pastry dough",
+    Measure = new Measure()
+    {
+        UnitType = UnitType.Volume,
+        Amount = 400
+    },
+    SubComponents = new List<ComponentRef>
+    {
+        new ComponentRef
+        {
+            Id = "2895c00b-e7dd-4cab-9446-5c2ec0c25a8c",
+            RefId = flour.Id,
+            Transformation = new MultiplyTransformation
+            {
+                Multiplier = 300
+            }
+        },
+        new ComponentRef
+        {
+            Id = "772315b6-b1fb-49d4-99df-17b5faeb397a",
+            RefId = sugarPowder.Id,
+            Transformation = new MultiplyTransformation
+            {
+                Multiplier = 100
+            }
+        },
+        new ComponentRef
+        {
+            Id = "fbf979c5-79d8-4bf4-9b9d-efb28b487ce2",
+            RefId = salt.Id,
+            Transformation = new MultiplyTransformation
+            {
+                Multiplier = 0.25
+            }
+        },
+        new ComponentRef
+        {
+            Id = "57869793-1858-4bb2-893d-b0a50d989806",
+            RefId = butter.Id,
+            Transformation = new MultiplyTransformation
+            {
+                Multiplier = 200
+            }
+        },
+        new ComponentRef
+        {
+            Id = "68d39763-635e-4a4f-bec5-c945c8c2fe52",
+            RefId = egg.Id
+        },
+    },
+    Process = new List<string>()
+    {
+        "Put {2895c00b-e7dd-4cab-9446-5c2ec0c25a8c}, {772315b6-b1fb-49d4-99df-17b5faeb397a}, {fbf979c5-79d8-4bf4-9b9d-efb28b487ce2} in a food processor and mix a little until homogenous",
+        "Add {57869793-1858-4bb2-893d-b0a50d989806} and mix in pulses until you get a sand like texture",
+        "Add {68d39763-635e-4a4f-bec5-c945c8c2fe52} and mix until getting a ball of dough. Don't mix too much.",
+        "Take out of the processor, wrap with plastic wrap and save in the fridge for 1-2 hours before use"
+    }
+};
+
+Recipe lemonCurd = new Recipe()
+{
+    Id = "f4e3d0b3-b85b-4b10-93bc-c2a6a5758395",
+    Name = "Lemon Curd",
+    Measure = new Measure()
+    {
+        UnitType = UnitType.Volume,
+        Amount = 200
+    },
+    SubComponents = new List<ComponentRef>
+    {
+        new ComponentRef
+        {
+            Id = "136302af-eb29-499f-aae1-6ad6eae04942",
+            RefId = lemonJuice.Id,
+            Transformation = new MultiplyTransformation
+            {
+                Multiplier = 100
+            }
+        },
+    }
+};
+
+Recipe lemonTartRecipe = new Recipe();
+lemonTartRecipe.Name = "Lemon Tart";
+lemonTartRecipe.Id = "ed2ff130-27b2-46e5-99b3-8cfae5d521b4";
+lemonTartRecipe.Measure = new TartMeasure
+{
+    Radius = 11,
+    Height = 2.5
+};
+
+lemonTartRecipe.SubComponents = new List<ComponentRef>
+{
+   
+    new ComponentRef
+    {
+        Id = "1ec7a68d-558b-4e77-b5e8-1d2a8c00f7e9",
+        RefId = pastryDough.Id,
+        Transformation = new MultiplyTransformation()
+        {
+            Multiplier = 0.5
+        }
+    }
+};
+
+RecipeMaster rm = new RecipeMaster
+{
+    Recipes = new List<Recipe>
+    {
+        lemonTartRecipe,
+        lemonCurd,
+        pastryDough
+    },
+    Ingredients = new List<Ingredient>
+    {
+        flour,
+        sugarPowder,
+        salt,
+        butter,
+        egg,
+        lemonJuice
+    }
+};
+
+rm.Build();
+//rm.PrintRecipe(lemonTartRecipe);
+pastryDough.PrintBasicRecipe();
+//lemonTartRecipe.PrintBasicRecipe();
+var set = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore};
+set.Converters.Add(new StringEnumConverter());
+Console.WriteLine(JsonConvert.SerializeObject(rm, Formatting.None, set));
+
+
+
+
+// Need to complete the getmeasure. For that we need to be able to join all the transformations into a single transformation (need e.g. transformation.merge maybe), and finally take the final unit of the ingredient.
+// This will allow showing simple recipes.
+// For complex we can use the assigned/override measure, but this will also need transformation merge.
---
+++ RecipeMaster1/Recipe.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeMaster1
+{
+    using Microsoft.VisualBasic.CompilerServices;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public enum UnitType
+    {
+        Gram,
+        Tbsp,
+        Item,
+        Volume
+    }
+
+    public abstract class RecipeEntity
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public abstract Measure GetMeasure();
+
+        public virtual string GetComponentName()
+        {
+            return Name;
+        }
+    }
+
+    public class IngredientsCollection
+    {
+        private Dictionary<string, (Ingredient Ing, Measure Measure)> m_ingList = new Dictionary<string, (Ingredient, Measure)>();
+
+        public Dictionary<string, (Ingredient Ing, Measure Measure)> Collection => m_ingList;
+
+        public void Add(Ingredient ing, Measure m)
+        {
+            if (m_ingList.ContainsKey(ing.Id))
+            {
+                m_ingList[ing.Id] = (ing, m + m_ingList[ing.Id].Measure);
+            }
+            else
+            {
+                m_ingList[ing.Id] = (ing, m);
+            }
+        }
+    }
+
+    public class Recipe : RecipeEntity
+    {
+        public List<ComponentRef> SubComponents { get; set; }
+
+        public List<string> Process { get; set; }
+
+        public Measure Measure { get; set; }
+
+        public string GenerateDetailedDescription()
+        {
+            return string.Empty;
+        }
+
+        public override Measure GetMeasure()
+        {
+            return Measure;
+        }
+
+        public IngredientsCollection GetTotalIngredients()
+        {
+            IngredientsCollection ingredients = new IngredientsCollection();
+
+            foreach (var component in SubComponents)
+            {
+                var remoteComponent = component.RemoteEntity;
+
+                if (remoteComponent is Ingredient ing)
+                {
+                    ingredients.Add(ing, component.GetMeasure());
+                }
+
+                if (remoteComponent is Recipe rcp)
+                {
+                    var remoteIngredients = rcp.GetTotalIngredients();
+                    foreach (var ingredient in remoteIngredients.Collection)
+                    {
+                        ingredients.Add(ingredient.Value.Ing, component.Transformation.GetMeasure(ingredient.Value.Measure));
+                    }
+                }
+            }
+
+            return ingredients;
+        }
+
+        public void PrintBasicRecipe()
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine($"Target measure: {Measure.Description}");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+            Console.WriteLine("= Ingredients:");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+            var ing = GetTotalIngredients();
+            foreach (var ingr in ing.Collection.Values)
+            {
+                Console.WriteLine(ingr.Measure.Description + " " + ingr.Ing.Name);
+            }
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+            Console.WriteLine("= components:");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+            foreach (var component in SubComponents)
+            {
+                var componentName = component.RemoteEntity.GetComponentName();
+                var measure = component.GetMeasure();
+
+                Console.WriteLine(measure.Description + " " + componentName);
+            }
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+            Console.WriteLine("= Process:");
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+
+            if (Process == null)
+            {
+                Console.WriteLine("Process not defined!`");
+            }
+            else
+            {
+                for (int i = 0; i < Process.Count; i++)
+                {
+                    Console.WriteLine($"{i}) {FormatProcessLine(Process[i])}");
+                }
+            }
+        }
+
+        private string FormatProcessLine(string s)
+        {
+            foreach (var comp in SubComponents)
+            {
+                s = s.Replace("{" + comp.Id + "}", $"{comp.GetMeasure().Description} {comp.GetComponentName()}");
+            }
+
+            return s;
+        }
+    }   
+
+    public class Measure
+    {
+        public UnitType UnitType { get; set; }
+
+        public double Amount { get; set; }
+
+        [JsonIgnore]
+        public virtual string Description
+        {
+            get
+            {
+                return $"{Amount} {UnitType}";
+            }
+        }
+
+        public static Measure operator+(Measure measureA, Measure measureB)
+        {
+            if (measureA.UnitType == measureB.UnitType)
+            {
+                return new Measure()
+                {
+                    UnitType = measureA.UnitType,
+                    Amount = measureA.Amount + measureB.Amount
+                };
+            }
+
+            throw new Exception($"Cant add unit {measureA.UnitType} with unit {measureB.UnitType}");
+        }
+    }
+
+    public class RoundMeasure
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class ComponentRef : RecipeEntity
+    {
+        public string RefId { get; set; }
+
+        public Transformation Transformation { get; set; } = null;
+
+        [JsonIgnore]
+        public RecipeEntity RemoteEntity { get; set; }
+
+        public override Measure GetMeasure()
+        {
+            if (Transformation == null)
+            {
+                return RemoteEntity.GetMeasure();
+            }
+            else
+            {
+                return Transformation.GetMeasure(RemoteEntity.GetMeasure());
+            }
+        }
+
+        public override string GetComponentName()
+        {
+            return RemoteEntity.Name;
+        }
+    }
+
+    public class Ingredient : RecipeEntity
+    {
+        public UnitType UnitType { get; set; }
+
+        public override Measure GetMeasure()
+        {
+            return new Measure()
+            {
+                UnitType = UnitType,
+                Amount = 1
+            };
+        }
+    }
+
+    public class Transformation
+    {
+        public virtual Measure GetMeasure(Measure m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class UnityTransformation : Transformation
+    {
+        public override Measure GetMeasure(Measure m)
+        {
+            return m;
+        }
+    }
+
+    public class TartMeasure : Measure
+    {
+        public double Radius { get; set; }
+        
+        public double Height { get; set; }
+
+        public TartMeasure()
+        {
+            UnitType = UnitType.Volume;
+        }
+
+        [JsonIgnore]
+        public override string Description
+        {
+            get
+            {
+                return $"Tart of radius {Radius}cm and height of {Height}cm ";
+            }
+        }
+    }
+
+    public class MultiplyTransformation : Transformation
+    {
+        public double Multiplier { get; set; }
+
+        public override Measure GetMeasure(Measure m)
+        {
+            return new Measure()
+            {
+                UnitType = m.UnitType,
+                Amount = m.Amount * Multiplier
+            };
+        }
+    }
+
+    public class RecipeMaster 
+    {
+        public List<Recipe> Recipes { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+
+        private Dictionary<string, RecipeEntity> m_entities;
+
+        public void PrintComplexRecipe(Recipe recipe)
+        {
+            Console.WriteLine(recipe.Name);
+            Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==");
+            Console.WriteLine("Components:");
+            foreach (var component in recipe.SubComponents)
+            {
+                var componentName = GetComponentName(component);
+
+                Console.WriteLine(componentName);
+            }
+        }
+
+       
+
+        private string GetMeasureName(ComponentRef component)
+        {
+            var desc = component.GetMeasure().Description;
+            return desc;
+        }
+
+        private string GetComponentName(RecipeEntity entity)
+        {
+            if (!string.IsNullOrEmpty(entity.Name))
+            {
+                return entity.Name;
+            }
+
+            if (entity is ComponentRef compRef)
+            {
+                return GetComponentName(m_entities[compRef.RefId]);
+            }
+
+            return "Unknown";
+        }
+
+        public void Build()
+        {
+            m_entities = new Dictionary<string, RecipeEntity>();
+            
+            foreach (var rcp in Recipes)
+            {
+                m_entities[rcp.Id] = rcp;
+                Console.WriteLine("Adding " + rcp.Id);
+            }
+
+            foreach (var ing in Ingredients)
+            {
+                m_entities[ing.Id] = ing;
+                Console.WriteLine("Adding " + ing.Id);
+            }
+
+            // Assign remote entities
+            foreach (var entity in m_entities.Values)
+            {
+                if (entity is Recipe recipe)
+                {
+                    foreach (var subCom in recipe.SubComponents)
+                    {
+                        if (subCom is ComponentRef compRef)
+                        {
+                            compRef.RemoteEntity = m_entities[compRef.RefId];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
